4ede6e06770692f17ccefec24e2807cd8274e2cd
Devangptl <devangmpatel51@gmail.com>
2024-02-24 00:44:37 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { FaApple } from "react-icons/fa";
+import { FaArrowRightLong } from "react-icons/fa6";
+import headerImg1 from "../Images/airpods-32431.png";
+import headerImg2 from "../Images/airpods-32433.png";
+import headerImg3 from "../Images/airpods-32448.png";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="  bg-[#000000]  h-[500px]   ">
+
+
+      <div className=" relative">
+        <div className=" text-white flex flex-col gap-4 absolute top-[100px] left-[200px]">
+          <p className=" flex gap-2 items-center text-[28px]">
+            {" "}
+            <span className=" text-[45px]">
+              <FaApple />
+            </span>{" "}
+            AirPods Pro Max{" "}
+          </p>
+
+          <p className="  text-white text-[80px] font-bold">
+            Up to 10% <p className=" mt-[-35px]">off Voucher</p> 
+          </p>
+
+        <Link to={"/"} className=" group flex items-center   gap-2  text-[25px] underline-offset-8 underline">Shop Now <span className=" group-hover:translate-x-2 duration-500 text-[30px] pt-2"><FaArrowRightLong/></span> </Link>
+        </div>
+      </div>
+
+      <div className="  relative">
+        
+        <img
+          width={400}
+          src={headerImg1}
+          alt=""
+          className="  duration-500 absolute top-[100px] left-[1000px] drop-shadow-2xl"
+        />
+        <img
+          width={120}
+          src={headerImg2}
+          alt=""
+          className=" hover:scale-110 duration-500 absolute top-[120px] left-[1230px] drop-shadow-2xl"
+        />
+        <img
+          width={150}
+          src={headerImg3}
+          alt=""
+          className=" hover:scale-110 duration-500 absolute top-[232px] left-[850px] drop-shadow-2xl "
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { GoHeart } from "react-icons/go";
+import { BsCart3 } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <div className=" border border-b-2 py-5  flex flex-row justify-around items-center">
+      <div className="  text-[24px] font-bold"> Gada Electronics</div>
+
+      <div className=" tracking-wide text-[#3c3c3c] text-lg   flex flex-row items-center gap-9">
+        <Link to={"/"} className=" duration-300 underline-offset-8 hover:underline  hover:text-orange-600">Home</Link>
+        <Link to={"/contact"} className="duration-300 underline-offset-8 hover:underline  hover:text-orange-600">Contact</Link>
+        <Link to={"/about"} className="duration-300 underline-offset-8 hover:underline  hover:text-orange-600">About</Link>
+        <Link to={"/signup"} className="duration-300 underline-offset-8 hover:underline  hover:text-orange-600">Sign Up</Link>
+      </div>
+
+      <div className=" text-[#3c3c3c]  flex flex-row items-center gap-7">
+        <div className=" flex flex-row items-center ">
+          <input
+            type="text"
+            id="password"
+            class=" text-[14px] outline-none pl-3 pr-10 py-2  bg-gray-100 rounded-[3px] hover:border-gray-300  "
+            placeholder="What are you looking for?"
+          />
+          <button class="  font-bold text-center text-[20px]   ml-[-30px]  text-gray-400  hover:text-gray-900 transition-colors">
+            <CiSearch />
+          </button>
+        </div>
+
+        <Link to={"/wishlist"} className=" pl-3 text-[24px]">
+          <GoHeart className=" hover:text-[#ac2121] " />
+        </Link>
+
+        <Link to={"/cart"} className=" text-[23px]">
+        {/* <FaOpencart /> */}
+        <BsCart3 />
+        
+        </Link>
+
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
